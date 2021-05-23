9f90ebacb358d25e4b829f85f76d40b713a04f15
halfmvsq <halfmvsq@gmail.com>
2021-05-22 18:33:34 -0600

    First go at polygon to segmentation mask fill algorithm

--- src/image/SegUtil.cpp
+++ src/image/SegUtil.cpp
@@ -7,11 +7,12 @@
 #include "logic/camera/MathUtility.h"
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
 
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/component_wise.hpp>
 #include <glm/gtx/hash.hpp>
-//#include <glm/gtx/string_cast.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
@@ -25,13 +26,22 @@
 namespace
 {
 
-// Does the voxel intersect the view plane?
-bool voxelInterectsViewPlane(
+// Does the voxel intersect a plane?
+// The plane is given in Voxel coordinates.
+bool voxelInterectsPlane(
         const glm::vec4& voxelViewPlane,
         const glm::vec3& voxelPos )
 {
     static const glm::vec3 cornerOffset{ 0.5f, 0.5f, 0.5f };
     return math::testAABBoxPlaneIntersection( voxelPos, voxelPos + cornerOffset, voxelViewPlane );
+}
+
+// Check if the voxel is inside the segmentation
+bool isVoxelInSeg( const glm::ivec3 segDims, const glm::ivec3& voxelPos )
+{
+    return ( voxelPos.x >= 0 && voxelPos.x < segDims.x &&
+             voxelPos.y >= 0 && voxelPos.y < segDims.y &&
+             voxelPos.z >= 0 && voxelPos.z < segDims.z );
 }
 
 
@@ -56,20 +66,12 @@ paintBrush2d(
     // Set of voxels that do not intersect with view plane or that should not be painted
     std::unordered_set< glm::ivec3 > voxelsToIgnore;
 
-    // Check if the voxel is inside the segmentation
-    auto isVoxelInSeg = [&segDims] ( const glm::ivec3& voxelPos )
-    {
-        return ( voxelPos.x >= 0 && voxelPos.x < segDims.x &&
-                 voxelPos.y >= 0 && voxelPos.y < segDims.y &&
-                 voxelPos.z >= 0 && voxelPos.z < segDims.z );
-    };
-
     // Insert the first voxel as a voxel to test, if it's inside the segmentation.
     // This voxel should intersect the view plane, since it was clicked by the mouse,
     // but test it to make sure.
 
-    if ( isVoxelInSeg( roundedPixelPos ) &&
-         voxelInterectsViewPlane( voxelViewPlane, roundedPixelPos ) )
+    if ( isVoxelInSeg( segDims, roundedPixelPos ) &&
+         voxelInterectsPlane( voxelViewPlane, roundedPixelPos ) )
     {
         voxelsToTest.push( roundedPixelPos );
         voxelsProcessed.insert( roundedPixelPos );
@@ -128,8 +130,8 @@ paintBrush2d(
             if ( 0 == voxelsProcessed.count( n ) &&
                  0 == voxelsToIgnore.count( n ) &&
                  0 == voxelsToPaint.count( n ) &&
-                 voxelInterectsViewPlane( voxelViewPlane, n ) &&
-                 isVoxelInSeg( n ) )
+                 voxelInterectsPlane( voxelViewPlane, n ) &&
+                 isVoxelInSeg( segDims, n ) )
             {
                 voxelsToTest.push( n );
                 voxelsProcessed.insert( n );
@@ -397,6 +399,7 @@ void paintSegmentation(
 }
 
 
+/// @todo Implement algorithm for filling smoothed polygons.
 void fillSegmentationWithPolygon(
         Image* seg,
         const Annotation* annot,
@@ -409,34 +412,34 @@ void fillSegmentationWithPolygon(
             const ComponentType& memoryComponentType, const glm::uvec3& offset,
             const glm::uvec3& size, const int64_t* data ) >& updateSegTexture )
 {
-//    static constexpr size_t OUTER_BOUNDARY = 0;
+    static constexpr size_t OUTER_BOUNDARY = 0;
 
-    //   const glm::ivec3 dims{ seg->header().pixelDimensions() };
     const glm::mat4& pixel_T_subject = seg->transformations().pixel_T_subject();
+    const glm::mat4& subject_T_pixel = seg->transformations().subject_T_pixel();
 
-    // Convert from space of the annotation plane to Subject space
+    // Convert from space of the annotation plane to segmentation pixel coordinates
     auto convertPointFromAnnotPlaneToRoundedSegPixelCoords =
             [&pixel_T_subject, &annot] ( const glm::vec2& annotPlanePos )
     {
         const glm::vec4 subjectPos{ annot->unprojectFromAnnotationPlaneToSubjectPoint( annotPlanePos ), 1.0f };
         const glm::vec4 pixelPos = pixel_T_subject * subjectPos;
-        const glm::vec3 pixelPos3 = pixelPos / pixelPos.w;
-        const glm::ivec3 roundedPixelPos{ glm::round( pixelPos3 ) };
-        return roundedPixelPos;
+        return glm::ivec3{ glm::round( glm::vec3{ pixelPos / pixelPos.w } ) };
     };
 
+    // Convert from segmentation pixel coordinates to the space of the annotation plane
+    auto convertPointFromSegPixelCoordsToAnnotPlane =
+            [&subject_T_pixel, &annot] ( const glm::vec3& pixelPos )
+    {
+        const glm::vec4 subjectPos = subject_T_pixel * glm::vec4{ pixelPos, 1.0f };
+        return annot->projectSubjectPointToAnnotationPlane( glm::vec3{ subjectPos / subjectPos.w } );
+    };
 
-    /// @todo Implement algorithm for filling smoothed polygons.
-
-//    const glm::vec3 segSpacing{ seg->header().spacing() };
-//    const glm::ivec3 segDims{ seg->header().pixelDimensions() };
 
     if ( ! annot->isClosed() || annot->isSmoothed() )
     {
         spdlog::warn( "Cannot fill annotation polygon that is not closed and not smoothed." );
         return;
     }
-
 
 
    // Min and max corners of the polygon AABB in annotation plane space:
@@ -446,64 +449,92 @@ void fillSegmentationWithPolygon(
    const glm::vec2 annotPlaneAabbMinCorner = aabb->first;
    const glm::vec2 annotPlaneAabbMaxCorner = aabb->second;
 
-   // Four corners of the AABB in annotation plane space:
-   const glm::vec2 annotPlaneCorner1{ annotPlaneAabbMinCorner.x, annotPlaneAabbMinCorner.y };
-   const glm::vec2 annotPlaneCorner2{ annotPlaneAabbMaxCorner.x, annotPlaneAabbMinCorner.y };
-   const glm::vec2 annotPlaneCorner3{ annotPlaneAabbMaxCorner.x, annotPlaneAabbMaxCorner.y };
-   const glm::vec2 annotPlaneCorner4{ annotPlaneAabbMinCorner.x, annotPlaneAabbMaxCorner.y };
+   const glm::ivec3 pixelAabbMinCorner =
+           convertPointFromAnnotPlaneToRoundedSegPixelCoords( annotPlaneAabbMinCorner );
 
-   // Convert the four corners to segmentation Pixel space (rounded):
-//   const glm::ivec3 pixelAabbMinCorner1 = convertPointFromAnnotPlaneToRoundedSegPixelCoords( annotPlaneCorner1 );
-//   const glm::ivec3 pixelAabbMaxCorner2 = convertPointFromAnnotPlaneToRoundedSegPixelCoords( annotPlaneCorner2 );
-//   const glm::ivec3 pixelAabbMaxCorner3 = convertPointFromAnnotPlaneToRoundedSegPixelCoords( annotPlaneCorner3 );
-//   const glm::ivec3 pixelAabbMaxCorner4 = convertPointFromAnnotPlaneToRoundedSegPixelCoords( annotPlaneCorner4 );
-
-   // Directions:
-//   const glm::vec3 pixelDirX = glm::normalize( glm::vec3{ pixelAabbMaxCorner2 - pixelAabbMinCorner1 } );
-//   const glm::vec3 pixelDirY = glm::normalize( glm::vec3{ pixelAabbMaxCorner4 - pixelAabbMinCorner1 } );
+   const glm::ivec3 pixelAabbMaxCorner =
+           convertPointFromAnnotPlaneToRoundedSegPixelCoords( annotPlaneAabbMaxCorner );
 
 
+   // Polygon vertices in the space of the annotation plane
+   const std::vector<glm::vec2>& annotPlaneVertices = annot->getBoundaryVertices( OUTER_BOUNDARY );
+   if ( annotPlaneVertices.empty() ) return;
 
-   std::vector< glm::ivec3 > voxelsToPaint;
 
-   for ( float y = annotPlaneAabbMinCorner.y; y <= annotPlaneAabbMaxCorner.y; y += 0.1f )
+   // Subject plane normal vector transformed into Voxel space:
+   const glm::vec3 pixelAnnotPlaneNormal = glm::normalize(
+               glm::inverseTranspose( glm::mat3( pixel_T_subject ) ) *
+               glm::vec3{ annot->getSubjectPlaneEquation() } );
+
+   // First vertex in Subject space, then in Pixel space:
+   const glm::vec3 subjectAnnotPlanePoint =
+           annot->unprojectFromAnnotationPlaneToSubjectPoint( annotPlaneVertices.front() );
+
+   const glm::vec4 pixelAnnotPlanePoint =
+           pixel_T_subject * glm::vec4{ subjectAnnotPlanePoint, 1.0f };
+
+   // Annotation plane in Pixel space:
+   const glm::vec4 pixelPlaneEquation = math::makePlane(
+               pixelAnnotPlaneNormal,
+               glm::vec3{ pixelAnnotPlanePoint / pixelAnnotPlanePoint.w } );
+
+
+   // Loop over the AABB in Pixel/Voxel space. Note that this is inefficient and tests
+   // too many voxels when the annotation plane is oblique in Voxel space.
+
+   const int minK = std::min( pixelAabbMinCorner.z, pixelAabbMaxCorner.z ) - 1;
+   const int maxK = std::max( pixelAabbMinCorner.z, pixelAabbMaxCorner.z ) + 1;
+
+   const int minJ = std::min( pixelAabbMinCorner.y, pixelAabbMaxCorner.y ) - 1;
+   const int maxJ = std::max( pixelAabbMinCorner.y, pixelAabbMaxCorner.y ) + 1;
+
+   const int minI = std::min( pixelAabbMinCorner.x, pixelAabbMaxCorner.x ) - 1;
+   const int maxI = std::max( pixelAabbMinCorner.x, pixelAabbMaxCorner.x ) + 1;
+
+
+   // Set of unique voxels to change:
+   std::unordered_set< glm::ivec3 > voxelsToChange;
+
+   const glm::ivec3 segDims{ seg->header().pixelDimensions() };
+
+   for ( int k = minK; k <= maxK; ++k )
    {
-       for ( float x = annotPlaneAabbMinCorner.x; x <= annotPlaneAabbMaxCorner.x; x += 0.1f )
+       for ( int j = minJ; j <= maxJ; ++j )
        {
-           const glm::ivec3 a = convertPointFromAnnotPlaneToRoundedSegPixelCoords( glm::vec2{ x, y } );
+           for ( int i = minI; i <= maxI; ++i )
+           {
+               const glm::ivec3 pixelPos{ i, j, k };
 
-           voxelsToPaint.push_back( a );
+               if ( ! isVoxelInSeg( segDims, pixelPos ) ) continue;
+
+               // Does the voxel intersect the annotation plane?
+               // This check is needed when the annotation plane is oblique in Pixel space
+               // due to our inefficient algorithm.
+
+               if ( ! voxelInterectsPlane( pixelPlaneEquation, glm::vec3{ pixelPos } ) ) continue;
+
+               const glm::vec2 annotPlanePos = convertPointFromSegPixelCoordsToAnnotPlane(
+                           glm::vec3{ pixelPos } );
+
+               if ( math::pnpoly( annotPlaneVertices, annotPlanePos ) )
+               {
+                   voxelsToChange.insert( pixelPos );
+               }
+           }
        }
    }
 
-//   if ( glm::any( glm::lessThan( roundedPixelPos, sk_voxelZero ) ) ||
-//        glm::any( glm::greaterThanEqual( roundedPixelPos, dims ) ) )
-//   {
-//       continue; // This pixel is outside the image
-//   }
+   // Min/max corners of the set of voxels to change
+   glm::ivec3 maxVoxel{ std::numeric_limits<int>::lowest() };
+   glm::ivec3 minVoxel{ std::numeric_limits<int>::max() };
 
+   for ( const auto& p : voxelsToChange )
+   {
+       minVoxel = glm::min( minVoxel, p );
+       maxVoxel = glm::max( maxVoxel, p );
+   }
 
-    // Polygon vertices in the space of the annotation plane
-//    const std::vector<glm::vec2>& annotPlaneVertices = annot->getBoundaryVertices( OUTER_BOUNDARY );
-
-
-    //    pnpoly
-
-    // Set of unique voxels to change:
-    std::unordered_set< glm::ivec3 > voxelsToChange;
-
-    // Min/max corners of the set of voxels to change
-    glm::ivec3 maxVoxel{ std::numeric_limits<int>::lowest() };
-    glm::ivec3 minVoxel{ std::numeric_limits<int>::max() };
-
-    for ( const auto& p : voxelsToPaint )
-    {
-        voxelsToChange.insert( p );
-        minVoxel = glm::min( minVoxel, p );
-        maxVoxel = glm::max( maxVoxel, p );
-    }
-
-    updateSeg( voxelsToChange, minVoxel, maxVoxel,
-               labelToPaint, labelToReplace, brushReplacesBgWithFg,
-               seg, updateSegTexture );
+   updateSeg( voxelsToChange, minVoxel, maxVoxel,
+              labelToPaint, labelToReplace, brushReplacesBgWithFg,
+              seg, updateSegTexture );
 }
